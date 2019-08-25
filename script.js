3f836fdfcc78f4fc0f20a064a280279dd50ba8eb
Eliasyoussef47 <d-s-a00@hotmail.com>
2019-08-25 18:49:27 +0200

    added an options button

--- script.js
+++ script.js
@@ -118,3 +118,13 @@ function convertNumber(element, operation) {
 	}
 	element.dispatchEvent(new Event('change', { bubbles: true }));
 }
+
+function openNav() {
+	document.getElementById("mySidebar").style.width = "250px";
+	//document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	//document.getElementById("main").style.marginLeft= "0";
+}
